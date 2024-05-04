f8b82527285536c285676d160404221a8f96dfff
Alex <you@example.com>
2024-05-04 10:27:32 +0500

    added achive block

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,7 +1,10 @@
-import logo1 from '../img/achive/logo-6.jpg'
-import logo2 from '../img/achive/logo-2.jpg'
-
 import styles from './Achievements.module.css'
+import achive1 from '../img/achive/achievement-1.jpg'
+import achive2 from '../img/achive/achievement-2.jpg'
+import achive3 from '../img/achive/achievement-3.jpg'
+import achive4 from '../img/achive/achievement-4.jpg'
+import achive5 from '../img/achive/achievement-5.jpg'
+import achive6 from '../img/achive/achievement-6.jpg'
 
 function Achievements() {
   return (
@@ -9,12 +12,24 @@ function Achievements() {
       <h2>
       Наши <span className={styles.redText}>достижения </span>
       </h2>
-      <div className="achiveItems">
+      <div className={styles.achiveItems}>
           <div className={styles.achiveItem}>
-            <img src={logo1} alt="" />
+            <img src={achive1} alt="" />
           </div>
-          <div className="achiveItem">
-            <img src={logo2} alt="" />
+          <div className={styles.achiveItem}>
+            <img src={achive2} alt="" />
+          </div>
+          <div className={styles.achiveItem}>
+            <img src={achive5} alt="" />
+          </div>
+          <div className={styles.achiveItem}>
+            <img src={achive4} alt="" />
+          </div>
+          <div className={styles.achiveItem}>
+            <img src={achive6} alt="" />
+          </div>
+          <div className={styles.achiveItem}>
+            <img src={achive3} alt="" />
           </div>
       </div>
 
